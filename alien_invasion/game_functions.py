60b781d12abacecb12450f9980cfe4416c9810a1
MatthewNewell006 <matthewnewell006@gmail.com>
2020-10-30 22:17:06 -0400

    Update

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -9,10 +9,13 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    # Create a new bullet and add it to the bullets group
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     """ Respond to key releases """
@@ -30,6 +33,31 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+
+def update_screen(ai_settings, screen, ship, bullets):
+    '''Update images on the screen and flip to the new screen'''
+    # Redraw the screen during each pass through the loop
+        # screen.fill(bg_color)
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # Make the most recently drawn screen visible
+    pygame.display.flip()
+
+    # Redraw all bullets behind ship and aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
+def update_bullets(bullets):
+    """ Update position of bullets and get rid of old bullets """
+    # Update bullet position
+    bullets.update()
+
+    # Get rid of bullets that have disappeared
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+    # print(len(bullets))
 
 
 # def check_events(ship):
@@ -52,17 +80,3 @@ def check_events(ai_settings, screen, ship, bullets):
 #                 ship.moving_right = False
 #             elif event.key == pygame.K_LEFT:
 #                 ship.moving_left = False
-
-def update_screen(ai_settings, screen, ship, bullets):
-    '''Update images on the screen and flip to the new screen'''
-    # Redraw the screen during each pass through the loop
-        # screen.fill(bg_color)
-    screen.fill(ai_settings.bg_color)
-    ship.blitme()
-
-    # Make the most recently drawn screen visible
-    pygame.display.flip()
-
-    # Redraw all bullets behind ship and aliens
-    for bullet in bullets.sprites():
-        bullet.draw_bullet()
