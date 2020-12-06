2e9cc8ea65a700b3d9cb09e554d7921ffc08ba5a
MatthewNewell006 <matthewnewell006@gmail.com>
2020-12-05 19:20:14 -0500

    Update

--- projects/alien_invasion/game_functions.py
+++ projects/alien_invasion/game_functions.py
@@ -39,7 +39,7 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     '''Update images on the screen and flip to the new screen'''
     # Redraw the screen during each pass through the loop
         # screen.fill(bg_color)
@@ -51,6 +51,10 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 
     ship.blitme()
     aliens.draw(screen)
+
+    # Draw the play button if the game is inactive
+    if not stats.game_active:
+        play_button.draw_button()
 
     # Make the most recently drawn screen visible
     pygame.display.flip()
--- projects/alien_invasion/game_stats.py
+++ projects/alien_invasion/game_stats.py
@@ -5,6 +5,9 @@ class GameStats():
         ''' Initialize statistics '''
         self.ai_settings = ai_settings
         self.reset_stats()
+
+        # Start game in an inactive state
+        self.game_active = False
     
     def reset_stats(self):
         ''' Initialize statistics that can change during the game. '''
